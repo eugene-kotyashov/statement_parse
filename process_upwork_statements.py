0ae91411392d52aa62a555f5bf8c6f870803a7b4
eugene-kotyashov <eugene.kotyashov@gmail.com>
2022-01-17 22:44:59 +0300

    added first report output

--- process_upwork_statements.py
+++ process_upwork_statements.py
@@ -22,7 +22,7 @@ regex_two = r"Service Fee[\w\d\s\W]+Amount: \$([\d]+.[\d]+) USD x[\w\d\s\W]+Note
 
 def process_first_invoice(invoice_text):
     '''
-    returns (invoic_id, date string, amount) on sucess,
+    returns (customer_info, invoice_id, date string, amount) on sucess,
     None otherwise 
     '''
     matches = re.finditer(regex_one, invoice_text, re.MULTILINE)
@@ -37,7 +37,7 @@ def process_first_invoice(invoice_text):
 
 def process_second_invoice(invoice_text):
     '''
-    returns (invoic_id, date string, amount) on sucess,
+    returns (total_amount, first_invoice, related_invoice_id, date string, fee_amount) on sucess,
     None otherwise 
     '''
     matches = re.finditer(regex_two, invoice_text, re.MULTILINE)
@@ -77,6 +77,15 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
+def convert_to_date_obj(date_string):
+    return datetime.datetime.strptime(date_string, '%b %d, %Y')
+
+def date_obj_to_string(date_obj):
+    return date_obj.strftime('%d.%m.%Y')
+
+def convert_date_string(date_string):
+    date_obj = datetime.datetime.strptime(date_string, '%b %d, %Y')
+    return date_obj.strftime('%d.%m.%Y')
 
 def get_usd_rate_on_data(date_string):
     date_obj = datetime.datetime.strptime(date_string, '%b %d, %Y')
@@ -109,19 +118,58 @@ for item in os.listdir(source_dir):  # loop through items in dir
         zip_ref.extractall(dir_name)  # extract file to dir
         zip_ref.close()  # close file
 
+first_records = []
+second_records = []
+
 for pdfFile in os.listdir(dir_name):
     invoice_text = extract_text(os.path.join(dir_name, pdfFile))
     res_sec = process_second_invoice(invoice_text)
     if res_sec:
-        print("processed second ", res_sec)
+        # print("processed second ", res_sec)
         date_str = res_sec[3]
         rate = get_usd_rate_on_data(date_str)
-        print("rate second: ", rate)
+        # print("rate second: ", rate)
+        second_records.append((
+            res_sec[0],
+            res_sec[1], res_sec[2], 
+            convert_to_date_obj(res_sec[3]),  res_sec[4], rate))
         # print('-------')
         # print(invoice_text)
         # print('-------')
     else:
         res_first = process_first_invoice(invoice_text)
-        print("processed first ", res_first)
+        # print("processed first ", res_first)
         rate = get_usd_rate_on_data(res_first[2])
-        print("rate first", rate)
+        # print("rate first", rate)
+        customer_info = re.sub(r"\s+", ' ', res_first[0])
+        customer_info = re.sub(r",", '', customer_info)
+        first_records.append((
+            customer_info,
+            res_first[1],
+            convert_to_date_obj(res_first[2]), res_first[3], rate))
+
+if len(first_records) > len(second_records):
+    print("got more of 1st invoices ")
+elif len(first_records) < len(second_records):
+    print("got more of 2nd invoices")
+else:
+    print("all invoices are matched")
+# print(first_records[-1])
+# print(second_records[-1])
+
+first_records = sorted(first_records, key=lambda rec: rec[2] )
+
+# print('first')
+
+second_records = sorted(second_records, key=lambda rec: rec[3] )
+# print('second ')
+
+# print required strings 
+for rec in first_records:
+    byn = float(rec[3])*float(rec[4])
+    print(
+        date_obj_to_string(rec[2]), ";",
+        rec[1] + " " + rec[0], ";",
+        round(byn, 2), ";",
+        "USD", ";",
+        rec[4])
